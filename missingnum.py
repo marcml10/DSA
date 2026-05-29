fc0e973bed5d09722a7636935b6f3d7eb2bd2c25
esaumuieh <markmlrave@gmail.com>
2026-05-29 09:06:56 +0530

    This is a new draft changed the logic

--- missingnum.py
+++ missingnum.py
@@ -1,6 +1,6 @@
 what = [1, 2, 4, 5, 6]
 summing=0
-overall=(len(what)*(len(what)+1))/2
+overall=((len(what)+1)*((len(what)+1)+1))//2
 for an in what:
     summing=summing+an
-print(summing-overall)
+print(overall - summing)
