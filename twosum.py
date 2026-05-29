f293a93fd389760f1e3dd0a6425589535773cb58
esaumuieh <markmlrave@gmail.com>
2026-05-29 10:50:44 +0530

    perfect answer

---
+++ twosum.py
@@ -0,0 +1,9 @@
+def twosum(numbers, target):
+    for i in range(len(numbers)):
+      for j in range(i+1,len(numbers)):
+        if numbers[i] + numbers [j] == target:
+          return (i,j)
+
+numbers = [2, 7, 11, 15]
+target = 9
+print(twosum(numbers, target))
