d21f2dada2a0e0f0410fc85aa94457da8ae92850
esaumuieh <markmlrave@gmail.com>
2026-08-13 19:53:12 +0530

    I did it pretty fast

---
+++ arrays_and_hashing/anagram.py
@@ -0,0 +1,16 @@
+def anagram(t:str , s:str) -> bool:
+    census = {}
+    if len(s) != len(t):
+        return False
+
+    for element in s:
+        census[element] = census.get(element, 0) + 1
+
+    for element in t:
+        if element not in census or census[element] == 0:
+            return False
+        census[element] -= 1
+        return True
+
+result = anagram("hitmo", "mohit")
+print(result)
